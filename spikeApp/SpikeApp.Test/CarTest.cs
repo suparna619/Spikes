0c5bd98dff61e8364c5f813ff47315dc6acc9129
Suparna Dey <suparna_dey@mckinsey.com>
2015-07-20 19:28:41 +0530

    adding NUnit framework

--- spikeApp/SpikeApp.Test/CarTest.cs
+++ spikeApp/SpikeApp.Test/CarTest.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Versioning;
 using NUnit.Framework;
 
 namespace SpikeApp.Test
 {
+    [TestFixture]
     class CarTest
     {
         [Test]
